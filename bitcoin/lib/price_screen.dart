2d782ca8a8407256efe8247ccc33a74f104b08c3
Takuma Kato <k.t.mifune@gmail.com>
2020-04-16 03:35:24 +0900

    ADD：ドロップダウンを変更

--- bitcoin/lib/price_screen.dart
+++ bitcoin/lib/price_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'coin_data.dart';
+import 'package:flutter/cupertino.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -9,18 +11,24 @@ class PriceScreen extends StatefulWidget {
 class _PriceScreenState extends State<PriceScreen> {
   String currentDropdownItemValue = 'USD';
 
-  void getDropdownItem() {
+  List<DropdownMenuItem<String>> getDropdownItem() {
+    String item;
+    List<DropdownMenuItem<String>> dropDownItemList = [];
+
     for (int i = 0; i < currenciesList.length; i++) {
-      print(currenciesList[i]);
+      item = currenciesList[i];
+      dropDownItemList.add(
+        DropdownMenuItem(child: Text(item), value: item),
+      );
     }
+    return dropDownItemList;
   }
 
   @override
   Widget build(BuildContext context) {
-    getDropdownItem();
     return Scaffold(
       appBar: AppBar(
-        title: Text('🤑 Coin Ticker'),
+        title: Text('Coin Ticker'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -29,7 +37,7 @@ class _PriceScreenState extends State<PriceScreen> {
           Padding(
             padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
             child: Card(
-              color: Colors.lightBlueAccent,
+              color: Colors.lightGreen,
               elevation: 5.0,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0),
@@ -51,33 +59,32 @@ class _PriceScreenState extends State<PriceScreen> {
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: DropdownButton<String>(
-              value: currentDropdownItemValue,
-              items: [
-                DropdownMenuItem(
-                  child: Text('USD'),
-                  value: 'USD',
-                ),
-                DropdownMenuItem(
-                  child: Text('URE'),
-                  value: 'URE',
-                ),
-                DropdownMenuItem(
-                  child: Text('GBP'),
-                  value: 'GBP',
-                )
-              ],
-              onChanged: (newValue) {
-                setState(() {
-                  currentDropdownItemValue = newValue;
-                  print(newValue);
-                });
-              },
-            ),
+            color: Colors.lightGreen,
+            child: CupertinoPicker(
+                backgroundColor: Colors.lightGreen,
+                itemExtent: 32.0,
+                onSelectedItemChanged: (selectedIndex) {
+                  print(selectedIndex);
+                },
+                children: [
+                  Text('USD'),
+                  Text('JPY'),
+                  Text('IND'),
+                ]),
           ),
         ],
       ),
     );
   }
 }
+
+//DropdownButton<String>(
+//value: currentDropdownItemValue,
+//items: getDropdownItem(),
+//onChanged: (newValue) {
+//setState(() {
+//currentDropdownItemValue = newValue;
+//print(newValue);
+//});
+//},
+//)
